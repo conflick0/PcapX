81732f1be1d8b0f6402f5aa0725743fb65d30ee5
conflick0 <jackhsu0099@gmail.com>
2025-02-07 10:31:13 +0800

    update match blacklist ip by file

--- 02_extract_blacklist_ip.py
+++ 02_extract_blacklist_ip.py
@@ -4,10 +4,7 @@ import pandas as pd
 from src.utils import EXTRACT_TYPE, extract_ip
 
 
-def get_match_blacklist_ip(report_endpoint_dir, black_list_pth):
-    # read blk ips
-    blk_ips = pd.read_csv(black_list_pth, header=None).to_numpy().flatten().tolist()
-
+def get_match_blacklist_ip_by_dir(report_endpoint_dir, black_list_pth):
     # get target files
     f_pths = glob.glob(os.path.join(report_endpoint_dir, '*.csv'))
 
@@ -16,6 +13,25 @@ def get_match_blacklist_ip(report_endpoint_dir, black_list_pth):
 
     # get match ip
     ips = out_df['ip'].values.tolist()
+
+    # export black list ip
+    export_match_blacklist_ip(ips, black_list_pth)
+
+    # save endpoint ip
+    out_df.to_csv(out_endpoint_csv, index=False)
+
+
+def get_match_blacklist_ip_by_file(csv_pth, black_list_pth):
+    df = pd.read_csv(csv_pth)
+    ips = df['ip'].values.tolist()
+    export_match_blacklist_ip(ips, black_list_pth)
+
+
+
+def export_match_blacklist_ip(ips, black_list_pth):
+    # read blk ips
+    blk_ips = pd.read_csv(black_list_pth, header=None).to_numpy().flatten().tolist()
+
     match_ips = list(set(blk_ips) & set(ips))
 
     # show info
@@ -28,16 +44,21 @@ def get_match_blacklist_ip(report_endpoint_dir, black_list_pth):
     match_ips = list(map(lambda x: [x], match_ips))
     pd.DataFrame(match_ips, columns=['ip']).to_csv(out_blk_csv, index=False)
 
-    # save endpoint ip
-    out_df.to_csv(out_endpoint_csv, index=False)
 
 
 if __name__ == '__main__':
-    black_list_pth = r'D:\Users\11user\Desktop\blacklist-2024.08.14\1.csv'
-    report_dir = r'D:\workspace\PcapX\report\test'
-    report_endpoint_dir = os.path.join(report_dir, 'endpoint')
+    black_list_pth = r'D:\blacklist-2025.01.22.csv.csv'
+    report_dir = r'D:\workspace\PcapX\report\tmp'
+
+    is_dir = False # mode use dir or file as inp
+    inp_endpoint_dir = os.path.join(report_dir, 'endpoint') # use dir as inp
+    inp_csv = r'D:\ip_list.csv' # or use file as inp
+
     out_blk_csv = os.path.join(report_dir, f'blk_ip.csv')
     out_endpoint_csv = os.path.join(report_dir, f'endpoint_ip.csv')
 
-    get_match_blacklist_ip(report_endpoint_dir, black_list_pth)
+    if is_dir:
+        get_match_blacklist_ip_by_dir(inp_endpoint_dir, black_list_pth)
+    else:
+        get_match_blacklist_ip_by_file(inp_csv, black_list_pth)
 
